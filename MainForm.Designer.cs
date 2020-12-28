e44c8f14485660584cf9b89430c49864702581ae
Romanito <romanito@gmail.com>
2020-12-28 15:57:26 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NeoFlyExport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.sfdGPX = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvLog = new System.Windows.Forms.DataGridView();
+            this.btnExportGPX = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfdGPX
+            // 
+            this.sfdGPX.DefaultExt = "gpx";
+            this.sfdGPX.Filter = "GPX files|*.gpx";
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.dgvLog, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.btnExportGPX, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.Size = new System.Drawing.Size(686, 390);
+            this.tableLayoutPanel.TabIndex = 2;
+            // 
+            // dgvLog
+            // 
+            this.dgvLog.AllowUserToAddRows = false;
+            this.dgvLog.AllowUserToDeleteRows = false;
+            this.dgvLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLog.Location = new System.Drawing.Point(3, 39);
+            this.dgvLog.Name = "dgvLog";
+            this.dgvLog.ReadOnly = true;
+            this.dgvLog.RowTemplate.Height = 25;
+            this.dgvLog.Size = new System.Drawing.Size(680, 359);
+            this.dgvLog.TabIndex = 1;
+            // 
+            // btnExportGPX
+            // 
+            this.btnExportGPX.Enabled = false;
+            this.btnExportGPX.Location = new System.Drawing.Point(3, 3);
+            this.btnExportGPX.Name = "btnExportGPX";
+            this.btnExportGPX.Size = new System.Drawing.Size(134, 30);
+            this.btnExportGPX.TabIndex = 2;
+            this.btnExportGPX.Text = "Export to GPX file";
+            this.btnExportGPX.UseVisualStyleBackColor = true;
+            this.btnExportGPX.Click += new System.EventHandler(this.btnExportGPX_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "NeoFlyExport";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SaveFileDialog sfdGPX;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.DataGridView dgvLog;
+        private System.Windows.Forms.Button btnExportGPX;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+﻿# NeoFlyExport
+
+Small program that reads (NeoFly)[https://www.neofly.net/]'s logbook and exports it as a GPS trace in a GPX file.
+
+
+## Credits
+
+NeoFlyExpert written by (Romanito)[https://github.com/Romanito].
+Icon made by (Freepik)[https://www.flaticon.com/authors/freepik].
+
+## Licenses
+
+NeoFlyExport is released under the [MIT licence](licence.txt).
